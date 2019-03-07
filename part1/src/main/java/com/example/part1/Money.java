0062ce0925337d8c2fc6b722571f9a587c3034d7
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-03-07 20:07:20 +0900

    chapter8

--- part1/src/main/java/com/example/part1/Money.java
+++ part1/src/main/java/com/example/part1/Money.java
@@ -1,7 +1,15 @@
 package com.example.part1;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
+	abstract Money times(int multipulier);
+	
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
