06f180adb7c68fc0c66289de9a0c9dbc21b1364c
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-03-07 20:37:20 +0900

    chapter9

--- part1/src/main/java/com/example/part1/Money.java
+++ part1/src/main/java/com/example/part1/Money.java
@@ -2,13 +2,24 @@ package com.example.part1;
 
 public abstract class Money {
 	protected int amount;
+	protected String currency;
 	abstract Money times(int multipulier);
+//	abstract String currency();
+	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	
 	public static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
+	}
+	
+	String currency() {
+		return currency;
 	}
 	
 	public boolean equals(Object object) {
--- part1/src/test/java/com/example/part1/MoneyTest.java
+++ part1/src/test/java/com/example/part1/MoneyTest.java
@@ -10,7 +10,7 @@ public class MoneyTest {
 
 	@Test
 	public void testMultiplication() {
-		Money five = new Dollar(5);
+		Money five = Money.dollar(5);
 		assertEquals(Money.dollar(10), five.times(2));
 		assertEquals(Money.dollar(15), five.times(3));
 	}
@@ -29,5 +29,11 @@ public class MoneyTest {
 		Money five = Money.franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
+	}
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(5).currency());
+		assertEquals("CHF", Money.franc(6).currency());
 	}
 }
