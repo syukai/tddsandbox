76eb67bcd890b6130e1b2762e269fdaf68d3a1fa
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-03-12 07:02:28 +0900

    chapter15

--- part1/src/main/java/com/example/part1/Expression.java
+++ part1/src/main/java/com/example/part1/Expression.java
@@ -1,5 +1,6 @@
 package com.example.part1;
 
 public interface Expression {
+	Expression plus(Expression addend);
 	Money reduce(Bank bank, String to);
 }
--- part1/src/main/java/com/example/part1/Sum.java
+++ part1/src/main/java/com/example/part1/Sum.java
@@ -1,15 +1,19 @@
 package com.example.part1;
 
 public class Sum implements Expression{
-	Money augend;
-	Money addend;
-	Sum(Money augend, Money addend) {
+	Expression augend;
+	Expression addend;
+	Sum(Expression augend, Expression addend) {
 		this.augend = augend;
 		this.addend = addend;
 	}
 	
 	public Money reduce(Bank bank, String to) {
-		int amount = this.augend.amount + this.addend.amount;
+		int amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount;
 		return new Money(amount, to);
+	}
+	
+	public Expression plus(Expression addend) {
+		return null;
 	}
 }
