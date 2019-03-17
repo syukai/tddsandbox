20e64f4b34769571d49dcfad5d5bb858c02d4a6d
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-03-17 11:23:08 +0900

    chapter20

--- part2/TestCase.py
+++ part2/TestCase.py
@@ -1,9 +1,13 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+        self.log = None
     def setUp(self):
+        pass
+    def tearDown(self):
         pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
--- part2/WasRun.py
+++ part2/WasRun.py
@@ -1,7 +1,8 @@
 from TestCase import TestCase
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
+    def tearDown(self):
+        self.log += "tearDown "
